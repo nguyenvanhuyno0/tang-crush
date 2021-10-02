aac2024f4f24bf9245dca019955d2e5122ef8c0a
nguyenvanhuyno0 <91787371+nguyenvanhuyno0@users.noreply.github.com>
2021-10-02 19:46:28 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,18 +1,18 @@
 const CONFIG = {
-    introTitle: 'Chị à!',
+    introTitle: 'Chị Nhi à!',
     introDesc: `Trái đất vốn lạ thường
     Mà sao Chị cứ đi nhầm đường
     Lạc vào tim Em lẻ loi
     Đằng sau chữ yêu đây là thương`,
     btnIntro: 'hihi',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên �',
+    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 🥰',
     desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
     btnYes: 'Vẫn cứ là thích Em <33',
-    btnNo: 'Không, Anh trai à :3',
-    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
+    btnNo: 'Không, Em à 🥺',
+    question:'Trên thế giới hơn 7 tỉ người mà sao em lại thích Chị <3',
     btnReply: 'Gửi cho Em <3',
     reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Em biết mà 🥰. Yêu Chị nhiều nhiều 😘😘',
+    mess: 'Em biết mà 🥰. Yêu Chị nhiều nhiều😛😛',
     messDesc: 'Em muốn rủ chị đi chơiiiiii.',
     btnAccept: 'Okiiiii lun <3',
     messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
