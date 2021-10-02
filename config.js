c35ed64107d670eb24134303648f336f2508c986
nguyenvanhuyno0 <91787371+nguyenvanhuyno0@users.noreply.github.com>
2021-10-02 19:49:24 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,9 +1,9 @@
 const CONFIG = {
     introTitle: 'Chị Nhi à!',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao Chị cứ đi nhầm đường
+    introDesc: `Chị đi kiểu gì
+    lạc vào đây mất rồii
     Lạc vào tim Em lẻ loi
-    Đằng sau chữ yêu đây là thương`,
+    ,
     btnIntro: 'hihi',
     title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 🥰',
     desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
